87f026f293551af9c9cc8cfc563bb1f973bfdfe1
henriquemgoncalves <157642024+henriquemgoncalves@users.noreply.github.com>
2024-03-13 14:31:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+const pg_1 = document.getElementById('pg_1');
+const pg_2 = document.getElementById('pg_2');
+const pg_3 = document.getElementById('pg_3');
+const pg_4 = document.getElementById('pg_4');
+const pg_5 = document.getElementById('pg_5');
+const pg_6 = document.getElementById('pg_6');
+const pg_7 = document.getElementById('pg_7');
+const pg_8 = document.getElementById('pg_8');
+const pg_9 = document.getElementById('pg_9');
+const pg_10 = document.getElementById('pg_10');
+
+pg_1.addEventListener('click', function(){
+    window.location.href = '1.html';
+});
+
+pg_2.addEventListener('click', function(){
+    window.location.href = '2.html';
+});
+
+pg_3.addEventListener('click', function(){
+    window.location.href = '3.html';
+});
+
+pg_4.addEventListener('click', function(){
+    window.location.href = '4.html';
+});
+
+pg_5.addEventListener('click', function(){
+    window.location.href = '5.html';
+});
+
+pg_6.addEventListener('click', function(){
+    window.location.href = '6.html';
+});
+
+pg_7.addEventListener('click', function(){
+    window.location.href = '7.html';
+});
+
+pg_8.addEventListener('click', function(){
+    window.location.href = '8.html'
+});
+
+pg_9.addEventListener('click', function(){
+    window.location.href = '9.html'
+});
+
+pg_10.addEventListener('click', function(){
+    window.location.href = '10.html'
+});
